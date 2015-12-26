8450ec800355227c409049a9f5af7c98bee33512
unknown <marcusavgreen@gmail.com>
2015-12-26 21:31:57 +0000

    first commit

---
+++ classes/event/qpractice_report_viewed.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The qpractice_viewed event.
+ *
+ * @package    mod_qpractice
+ * @copyright  2015 Marcus Green
+ * @since     Moodle 2.9
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace qpractice\attempt;
+
+defined('MOODLE_INTERNAL') || die();
+
+namespace mod_qpractice\event;
+
+class qpractice_report_viewed extends \core\event\base {
+
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'qpractice';
+    }
+
+    public function get_url() {
+        return new \moodle_url('/mod/qpractice/report.php', array('id' => $this->objectid));
+    }
+
+    public function get_description() {
+        return "The user with id {$this->userid} viewed the report for qpractice id :  {$this->objectid}.";
+    }
+
+}
