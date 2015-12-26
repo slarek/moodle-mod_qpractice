79a8c496b4454c954705b991ebacdbb56bad7c3d
unknown <marcusavgreen@gmail.com>
2015-12-26 21:32:30 +0000

    first commit

--- classes/event/qpractice_viewed.php
+++ classes/event/qpractice_viewed.php
@@ -13,55 +13,36 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 /**
  * The qpractice_viewed event.
  *
+ * @since     Moodle 2.9
  * @package    mod_qpractice
  * @copyright  2015 Marcus Green
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 namespace qpractice\viewed;
+
 defined('MOODLE_INTERNAL') || die();
-/**
- * The qpractice_viewed  event class.
- *
- * @property-read array $other {
- *      Extra information about event.
- *
- *      - PUT INFO HERE
- * }
- *
- * @since     Moodle 2.9
- * @copyright 2015 Marcus Green
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- **/
+
 namespace mod_qpractice\event;
 
 class qpractice_viewed extends \core\event\base {
+
     protected function init() {
-        //$this->data['crud'] = 'c'; // c(reate), r(ead), u(pdate), d(elete)
-        //$this->data['edulevel'] = self::LEVEL_PARTICIPATING;
-        $this->data['objecttable'] = 'mod_qpractice';
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'qpractice';
     }
- 
-    public static function get_name() {
-        return get_string('eventqpractice_viewed', 'mod_qpractice');
-    }
- 
+
     public function get_description() {
-        //return "The user with id {$this->userid} viewed qpractice id :  {$this->objectid}.";
-        return "mavg";
+        return "The user with id {$this->userid} viewed qpractice id :  {$this->objectid}.";
     }
- 
+
     public function get_url() {
-        return "mavg";
-        //return new \moodle_url('/mod/qpractice/view.php', array('view' => $this->objectid));
+        return new \moodle_url('/mod/qpractice/view.php', array('id' => $this->courseid));
     }
- 
-    public function get_legacy_logdata() {
-        return "mavg";
-        //return array($course->id, 'qpractice', 'view', "view.php?id={$cm->id}", $qpractice->id, $cm->id);        
-    }
- 
+
 }
